77b69e5d194607e0459f631285962d3deec75885
Azeez Israel <111007711+yungstarry@users.noreply.github.com>
2024-03-11 12:07:28 +0100

    Add files via upload

--- SQLQuery(Taofiq).sql
+++ SQLQuery(Taofiq).sql
@@ -239,3 +239,177 @@ cross join department d
 select e.emp_name, e.salary, c.company_name, c.location
 from employee e
 cross join company c
+
+
+--use inner join  instead of Natural JOin
+
+--self join
+--self join is when u join a table to it self
+
+create table family (
+member_id varchar(10),
+name varchar(50),
+age int,
+parent_id varchar(10)
+)
+
+insert into family values
+('F1', 'David', 4, 'F5'),
+('F2', 'Carol', 10, 'F5'),
+('F3', 'Micheal', 12, 'F5'),
+('F4', 'Johnson', 36, '' ),
+('F5', 'Maryam', 40, 'F6'),
+('F6', 'Stewart', 70, ''),
+('F7', 'Rohan', 6, 'F4'),
+('F8', 'Asha', 8, 'F4')
+
+select * from family
+
+
+select child.name as child_name, child.age as child_age, parent.name as parent_name, parent.age as parent_age
+from family as child
+join family as parent on child.parent_id = parent.member_id
+
+DROP TABLE employee_history;
+DROP TABLE employee;
+drop table department;
+DROP table sales ;
+
+create table department
+(
+	dept_id		int ,
+	dept_name	varchar(50) PRIMARY KEY,
+	location	varchar(100)
+);
+insert into department values (1, 'Admin', 'Bangalore');
+insert into department values (2, 'HR', 'Bangalore');
+insert into department values (3, 'IT', 'Bangalore');
+insert into department values (4, 'Finance', 'Mumbai');
+insert into department values (5, 'Marketing', 'Bangalore');
+insert into department values (6, 'Sales', 'Mumbai');
+
+CREATE TABLE EMPLOYEE
+(
+    EMP_ID      INT PRIMARY KEY,
+    EMP_NAME    VARCHAR(50) NOT NULL,
+    DEPT_NAME   VARCHAR(50) NOT NULL,
+    SALARY      INT,
+    constraint fk_emp foreign key(dept_name) references department(dept_name)
+);
+
+insert into employee values(101, 'Mohan', 'Admin', 4000);
+insert into employee values(102, 'Rajkumar', 'HR', 3000);
+insert into employee values(103, 'Akbar', 'IT', 4000);
+insert into employee values(104, 'Dorvin', 'Finance', 6500);
+insert into employee values(105, 'Rohit', 'HR', 3000);
+insert into employee values(106, 'Rajesh',  'Finance', 5000);
+insert into employee values(107, 'Preet', 'HR', 7000);
+insert into employee values(108, 'Maryam', 'Admin', 4000);
+insert into employee values(109, 'Sanjay', 'IT', 6500);
+insert into employee values(110, 'Vasudha', 'IT', 7000);
+insert into employee values(111, 'Melinda', 'IT', 8000);
+insert into employee values(112, 'Komal', 'IT', 10000);
+insert into employee values(113, 'Gautham', 'Admin', 2000);
+insert into employee values(114, 'Manisha', 'HR', 3000);
+insert into employee values(115, 'Chandni', 'IT', 4500);
+insert into employee values(116, 'Satya', 'Finance', 6500);
+insert into employee values(117, 'Adarsh', 'HR', 3500);
+insert into employee values(118, 'Tejaswi', 'Finance', 5500);
+insert into employee values(119, 'Cory', 'HR', 8000);
+insert into employee values(120, 'Monica', 'Admin', 5000);
+insert into employee values(121, 'Rosalin', 'IT', 6000);
+insert into employee values(122, 'Ibrahim', 'IT', 8000);
+insert into employee values(123, 'Vikram', 'IT', 8000);
+insert into employee values(124, 'Dheeraj', 'IT', 11000);
+
+
+CREATE TABLE employee_history
+(
+    emp_id      INT PRIMARY KEY,
+    emp_name    VARCHAR(50) NOT NULL,
+    dept_name   VARCHAR(50),
+    salary      INT,
+    location    VARCHAR(100),
+    constraint fk_emp_hist_01 foreign key(dept_name) references department(dept_name),
+    constraint fk_emp_hist_02 foreign key(emp_id) references employee(emp_id)
+);
+
+create table sales
+(
+	store_id  		int,
+	store_name  	varchar(50),
+	product_name	varchar(50),
+	quantity		int,
+	price	     	int
+);
+insert into sales values
+(1, 'Apple Store 1','iPhone 13 Pro', 1, 1000),
+(1, 'Apple Store 1','MacBook pro 14', 3, 6000),
+(1, 'Apple Store 1','AirPods Pro', 2, 500),
+(2, 'Apple Store 2','iPhone 13 Pro', 2, 2000),
+(3, 'Apple Store 3','iPhone 12 Pro', 1, 750),
+(3, 'Apple Store 3','MacBook pro 14', 1, 2000),
+(3, 'Apple Store 3','MacBook Air', 4, 4400),
+(3, 'Apple Store 3','iPhone 13', 2, 1800),
+(3, 'Apple Store 3','AirPods Pro', 3, 750),
+(4, 'Apple Store 4','iPhone 12 Pro', 2, 1500),
+(4, 'Apple Store 4','MacBook pro 16', 1, 3500);
+
+select * from employee;
+select * from department;
+select * from employee_history;
+select * from sales;
+
+/* Question find the employees who's salary is more than average salary by all employee
+
+*/
+
+select *
+from EMPLOYEE e
+where e.SALARY >(select Avg(salary) as avg_sal 
+from EMPLOYEE )
+
+--types of subqueries
+--scalar subquery -- will always return one column and one row
+
+
+select *
+from EMPLOYEE e
+join (select Avg(salary) as sal
+from EMPLOYEE) avg_sal 
+on e.SALARY > avg_sal.sal
+
+
+--mulltiple row subquery -- returns multiple column and multiple row or one column multiple column
+
+-- Question: Find the employee who earn higest salary in each department
+
+select e.DEPT_NAME, MAX(e.salary) as Max_Salary
+from EMPLOYEE e
+group by e.dept_name
+
+select *
+from EMPLOYEE
+where exists(DEPT_NAME,salary) (select e.DEPT_NAME, MAX(e.salary) as Max_Salary
+from EMPLOYEE e
+group by e.dept_name)
+having DEPT_NAME = SALARY
+
+
+
+
+SELECT E1.*
+FROM EMPLOYEE E1
+WHERE EXISTS (
+    SELECT *
+    FROM EMPLOYEE E2
+    WHERE E1.DEPT_NAME = E2.DEPT_NAME
+    GROUP BY DEPT_NAME
+    HAVING MAX(E2.SALARY) = E1.SALARY
+);
+
+
+
+
+
+--correlated subquery
