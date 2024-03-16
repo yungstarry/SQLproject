31b372d48092bab685ccaff14934f0078eadf267
Azeez Israel <111007711+yungstarry@users.noreply.github.com>
2024-03-16 16:10:23 +0100

    Add files via upload

--- SQLQuery(Taofiq).sql
+++ SQLQuery(Taofiq).sql
@@ -705,7 +705,7 @@ FROM EMPLOYEE e
  from EMPLOYEE e
 
 
- --first value, last value, nth_value, ntile, percent_rank, cume_dist
+ --first value, last value, 
 
 
  create table product (
@@ -764,3 +764,73 @@ FROM EMPLOYEE e
  first_value(product_name) over(partition by product_category order by price desc) as Most_exp_product,
  last_value(product_name) over(partition by product_category order by price desc range between unbounded preceding and unbounded following) as least_exp_product
  from product
+
+ --today
+ --alternate why of write queries
+
+ 
+ select * ,
+	 first_value(product_name) over w as Most_exp_product,
+	 last_value(product_name) over w as least_exp_product
+ from product
+	where product_category='Phone'
+window w as(partition by product_category order by price desc range between unbounded preceding and unbounded following)
+
+--nth_value
+--write query to display the second most expensive product under each category
+
+ select * ,
+	 first_value(product_name) over w as Most_exp_product,
+	 last_value(product_name) over w as least_exp_product
+	-- nth_value(product_name, 2) over w as second_most_exp_product
+ from product
+	where product_category='Phone'
+window w as(partition by product_category order by price desc range between unbounded preceding and unbounded following)
+
+
+--NTILE
+--Write a query to segregate all the expensive phones, mid range phones and the cheaper phones
+
+select product_name, 
+case 
+	when x.buckets = 1 then 'Expensive Phones'
+	when x.buckets = 2 then 'Mid Range Phones'
+	when x.buckets = 3 then 'Cheapere Phones'
+END phone_category
+from(
+	select *,
+	ntile(3) over(order by price desc) as buckets
+	from product
+	where product_category ='phone') x
+
+--Cume_Dist(cumulatiove distribution);
+--Value --> <= CUME_Dsist > 0 
+--Formula - Current Row no (or Row No with value same as current row) /Total no of rows 
+
+--Query to fetch all products with are constituting the first 30%
+--of the data in products table based on price
+
+
+select product_name, (cume_dis_percentage) as cume_dis_percentage
+from 
+		(select * ,
+		cume_dist() over (order by price desc )as cume_distribution,
+		round(cume_dist() over (order by price desc )  * 100, 2) as cume_dis_percentage
+		from product) x
+
+where x.cume_dis_percentage <= 30;
+
+
+--Percent_rank(relative rank of the cuttent row/ percentage ranking)
+--vale ---> 1 <= PERCENT_RANK > 0
+--formuar = current row no -1 total no of rows -1
+
+
+--Query to identify how much percentage more expensive is "Galaxy z Fold 3" whne compare to all products
+
+
+select *,
+percent_rank() over(order by price) as percentage_rank,
+round(percent_rank() over(order by price) * 100, 2 )as percentage_rank
+from product
+
