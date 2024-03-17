7920d99d5785d542ee48baeabbf873fe267da2a6
Azeez Israel <111007711+yungstarry@users.noreply.github.com>
2024-03-17 10:02:23 +0100

    views

--- SQLQuery(Taofiq).sql
+++ SQLQuery(Taofiq).sql
@@ -834,3 +834,126 @@ percent_rank() over(order by price) as percentage_rank,
 round(percent_rank() over(order by price) * 100, 2 )as percentage_rank
 from product
 
+
+--VIEW
+--View is a  database object which is created over an SQL Query
+--View return data that is return by an sql query, view does not store any data, view is like a virtual data
+
+drop table if exists tb_customer_data ;
+drop table if exists tb_product_info ;
+drop table if exists tb_order_details ;
+drop view if exists order_summary;
+
+
+if not exists ( SELECT * FROM sys.tables WHERE name = 'tb_product_info')
+if not exists ( SELECT * FROM sys.tables WHERE name = 'tb_product_info')
+if not exists ( SELECT * FROM sys.tables WHERE name = 'tb_order_details')
+if not exists ( SELECT * FROM sys.tables WHERE name = 'order_summary')
+
+create table  tb_customer_data
+(
+    cust_id         varchar(10) primary key,
+    cust_name       varchar(50) not null,
+    phone           bigint,
+    email           varchar(50),
+    address         varchar(250)
+);
+
+create table tb_product_info
+(
+    prod_id         varchar(10) primary key,
+    prod_name       varchar(50) not null,
+    brand           varchar(50) not null,
+    price           int
+);
+
+create table  tb_order_details
+(
+    ord_id          bigint primary key,
+    prod_id         varchar(10) references tb_product_info(prod_id),
+    quantity        int,
+    cust_id         varchar(10) references tb_customer_data(cust_id),
+    disc_percent    int,
+    date            date
+);
+
+insert into tb_customer_data values
+('C1', 'Mohan Kumar', 9900807090, 'mohan@demo.com', 'Bangalore'),
+('C2', 'James Xavier', 8800905544, 'james@demo.com', 'Mumbai'),
+('C3', 'Priyanka Verma', 9900223333, 'priyanka@demo.com', 'Chennai'),
+('C4', 'Eshal Maryam', 9900822111, 'eshal@demo.com', 'Delhi');
+
+insert into tb_product_info values
+('P1', 'Samsung S22', 'Samsung', 800),
+('P2', 'Google Pixel 6 Pro', 'Google', 900),
+('P3', 'Sony Bravia TV', 'Sony', 600),
+('P4', 'Dell XPS 17', 'Dell', 2000),
+('P5', 'iPhone 13', 'Apple', 800),
+('P6', 'Macbook Pro 16', 'Apple', 5000);
+
+insert into tb_order_details values
+(1, 'P1', 2, 'C1', 10, '01-01-2020'),
+(2, 'P2', 1, 'C2', 0, '01-01-2020'),
+(3, 'P2', 3, 'C3', 20, '02-01-2020'),
+(4, 'P3', 1, 'C1', 0, '02-01-2020'),
+(5, 'P3', 1, 'C1', 0, '03-01-2020'),
+(6, 'P3', 4, 'C1', 25, '04-01-2020'),
+(7, 'P3', 1, 'C1', 0, '05-01-2020'),
+(8, 'P5', 1, 'C2', 0, '02-01-2020'),
+(9, 'P5', 1, 'C3', 0, '03-01-2020'),
+(10, 'P6', 1, 'C2', 0, '05-01-2020'),
+(11, 'P6', 1, 'C3', 0, '06-01-2020'),
+(12, 'P6', 5, 'C1', 30, '07-01-2020');
+
+
+select * from tb_customer_data;
+select * from tb_product_info;
+select * from tb_order_details;
+
+--fetch the order summary(to be given to client/vendor)
+IF OBJECT_ID('order_summary', 'V') IS NOT NULL
+    DROP VIEW order_summary;
+GO
+create view order_summary
+as
+select o.ord_id, o.date, p.prod_name, c.cust_name, (p.price * o.quantity) - ((p.price * o.quantity) * disc_percent /100) as cost
+from tb_customer_data c
+join tb_order_details o on o.cust_id = c.cust_id
+join tb_product_info p on p.prod_id = o.prod_id
+
+select * from order_summary
+
+--what is the main purpose of using a view/advantages of views
+--security
+--to simplify complex sql queries
+
+--updatable views is not advisable
+--rules of updatable table
+--1 View query formed using just 1 table/view
+--2 cannot have distinct clase
+--3 view query cannot have groupby clause
+-- view query cannot have with clause or window functions
+
+
+
+create view expensive_products
+as
+select * from tb_product_info where price > 1000
+select * from expensive_products
+
+update expensive_products
+set prod_name = 'Airpods pro' , brand= 'Apple'
+where prod_id = 'p4'
+
+drop view apple_products
+
+---With Check Option
+create view apple_products
+as
+select * from tb_product_info where brand = 'Apple'
+with check option
+
+select * from apple_products
+select * from tb_product_info
+insert into apple_products values
+('P22', 'Note 20', 'Apple', 2500);
