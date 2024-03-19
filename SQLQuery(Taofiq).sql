6a072b7dd38658f31a1f745e3a2b9cc87518a98b
Azeez Israel <111007711+yungstarry@users.noreply.github.com>
2024-03-19 11:12:37 +0100

    Add files via upload

--- SQLQuery(Taofiq).sql
+++ SQLQuery(Taofiq).sql
@@ -1169,3 +1169,190 @@ select customer
   end as Total
 
 from final_data
+
+
+
+--Interview Questions
+
+--- Q1: Delete duplicate data --- 
+
+drop table if exists cars;
+create table cars
+(
+	model_id		int primary key,
+	model_name		varchar(100),
+	color			varchar(100),
+	brand			varchar(100)
+);
+insert into cars values(1,'Leaf', 'Black', 'Nissan');
+insert into cars values(2,'Leaf', 'Black', 'Nissan');
+insert into cars values(3,'Model S', 'Black', 'Tesla');
+insert into cars values(4,'Model X', 'White', 'Tesla');
+insert into cars values(5,'Ioniq 5', 'Black', 'Hyundai');
+insert into cars values(6,'Ioniq 5', 'Black', 'Hyundai');
+insert into cars values(7,'Ioniq 6', 'White', 'Hyundai');
+
+select * from cars;
+
+delete from cars
+where model_id not in (select min(model_id)
+from cars
+group by model_name)
+
+
+--- Q2: Display highest and lowest salary --- 
+
+if OBJECT_ID('employees', 'U') is not null 
+drop table employees;
+
+create table employees
+(
+	id 			int primary key identity,
+	name 		varchar(100),
+	dept 		varchar(100),
+	salary 		int
+);
+insert into employees values( 'Alexander', 'Admin', 6500);
+insert into employees values( 'Leo', 'Finance', 7000);
+insert into employees values( 'Robin', 'IT', 2000);
+insert into employees values( 'Ali', 'IT', 4000);
+insert into employees values( 'Maria', 'IT', 6000);
+insert into employees values( 'Alice', 'Admin', 5000);
+insert into employees values( 'Sebastian', 'HR', 3000);
+insert into employees values( 'Emma', 'Finance', 4000);
+insert into employees values( 'John', 'HR', 4500);
+insert into employees values( 'Kabir', 'IT', 8000);
+
+select * from employees;
+
+select e.dept, max(salary)
+from employees e
+group by e.dept
+
+select *,
+max(salary) over(partition by dept order by salary desc) as highest_sal,
+min(salary) over(partition by dept order by salary desc 
+				 range between unbounded preceding and unbounded following) as lowest_sal
+from employees
+
+--- Q3 : Find actual distance --- 
+if OBJECT_ID('car_travels', 'U') is not null
+drop table car_travels;
+create table car_travels
+(
+    cars                    varchar(40),
+    days                    varchar(10),
+    cumulative_distance     int
+);
+insert into car_travels values ('Car1', 'Day1', 50);
+insert into car_travels values ('Car1', 'Day2', 100);
+insert into car_travels values ('Car1', 'Day3', 200);
+insert into car_travels values ('Car2', 'Day1', 0);
+insert into car_travels values ('Car3', 'Day1', 0);
+insert into car_travels values ('Car3', 'Day2', 50);
+insert into car_travels values ('Car3', 'Day3', 50);
+insert into car_travels values ('Car3', 'Day4', 100);
+
+select * from car_travels;
+
+select *, cumulative_distance - lag(cumulative_distance, 1, 0) over(partition by cars order by days) as distance_covered
+from car_travels
+
+
+--- Q4 : Convert the given input to expected output --- 
+if OBJECT_ID('src_dest_distance', 'u') is not null
+drop table src_dest_distance;
+create table src_dest_distance
+(
+    source          varchar(20),
+    destination     varchar(20),
+    distance        int
+);
+insert into src_dest_distance values ('Bangalore', 'Hyderbad', 400);
+insert into src_dest_distance values ('Hyderbad', 'Bangalore', 400);
+insert into src_dest_distance values ('Mumbai', 'Delhi', 400);
+insert into src_dest_distance values ('Delhi', 'Mumbai', 400);
+insert into src_dest_distance values ('Chennai', 'Pune', 400);
+insert into src_dest_distance values ('Pune', 'Chennai', 400);
+
+select * from src_dest_distance;
+with cte as (
+			select *, ROW_NUMBER() over( order by distance) as rn
+			from src_dest_distance
+)
+select t1.source, t1.destination, t1.distance
+from cte t1
+join cte t2
+			on t1.rn < t2.rn
+			and t1.source = t2.destination
+			and t1.destination =  t2.source
+
+
+--- Q5 : Ungroup the given input data --- 
+
+if OBJECT_ID('travel_items', 'u') is not null
+drop table travel_items;
+create table travel_items
+(
+	id              int,
+	item_name       varchar(50),
+	total_count     int
+);
+insert into travel_items values (1, 'Water Bottle', 2);
+insert into travel_items values (2, 'Tent', 1);
+insert into travel_items values (3, 'Apple', 4);
+			
+select * from travel_items
+
+with cte as
+		(select id, item_name, total_count, 1 as level
+		from travel_items
+		union all
+		select cte.id, cte.item_name, cte.total_count -1, level+1 as level
+		from cte
+		join travel_items t on t.item_name = cte.item_name and t.id = cte.id
+		where cte.total_count > 1
+		)
+		select id, item_name, level
+		from cte
+
+
+--- Q6 : IPL Matches --- 
+
+if OBJECT_ID('teams', 'u') is not null
+drop table teams;
+create table teams
+    (
+        team_code       varchar(10),
+        team_name       varchar(40)
+    );
+
+insert into teams values ('RCB', 'Royal Challengers Bangalore');
+insert into teams values ('MI', 'Mumbai Indians');
+insert into teams values ('CSK', 'Chennai Super Kings');
+insert into teams values ('DC', 'Delhi Capitals');
+insert into teams values ('RR', 'Rajasthan Royals');
+insert into teams values ('SRH', 'Sunrisers Hyderbad');
+insert into teams values ('PBKS', 'Punjab Kings');
+insert into teams values ('KKR', 'Kolkata Knight Riders');
+insert into teams values ('GT', 'Gujarat Titans');
+insert into teams values ('LSG', 'Lucknow Super Giants');
+
+select * from teams
+
+with matches as 
+				(select ROW_NUMBER() over(order by team_name) as id, t.*
+				from teams t)
+	select team.team_name as team, opponent.team_name as opponent
+	from matches team
+	join matches opponent on team.id < opponent.id
+	order by team
+
+
+with matches as 
+				(select ROW_NUMBER() over(order by team_name) as id, t.*
+				from teams t)
+	select team.team_name as team, opponent.team_name as opponent
+	from matches team
+	join matches opponent on team.id <> opponent.id
+	order by team
